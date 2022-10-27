b6b8142b3de1009e473057c26d777301f3e28b9f
Abdul-Qadir SANNY <qadirsanny@gmail.com>
2022-10-27 11:49:17 +0200

    get file size to send from uer input

--- client.java
+++ client.java
@@ -4,19 +4,33 @@ import java.io.*;
 public class client
 {
     public static void main(String args[])throws Exception
-    {         
-        byte b[]=new byte[135000];
-        
-        FileInputStream f=new FileInputStream("test.txt");
+    {    
+        int serveurPort = 3333;
+        InetAddress serverAddress = InetAddress.getLocalHost();
+        String file = "test.txt";
+        int size = 135000;
+
+        if (args.length == 1){
+            size = Integer.parseInt(args[0]);
+        }
+        if (args.length == 3){
+            // we got the port and the filename
+            serverAddress = InetAddress.getByName(args[1]);
+            serveurPort = Integer.parseInt(args[1]);
+            file = args[2];
+        }
+
+        byte b[]=new byte[size];
+        FileInputStream fop=new FileInputStream(file);
         DatagramSocket dsoc=new DatagramSocket(3331);
         int i=0;
-        while(f.available()!=0)
+        while(fop.available()!=0)
         {
-            b[i]=(byte)f.read();
+            b[i]=(byte)fop.read();
             i++;
         }                     
-        f.close();
-        dsoc.send(new DatagramPacket(b,i,InetAddress.getLocalHost(),3333));
+        fop.close();
+        dsoc.send(new DatagramPacket(b, i, serverAddress, serveurPort));
     }
 }
 
--- server.java
+++ server.java
@@ -1,29 +1,30 @@
 /*SERVER CLASS*/
 import java.net.*;
-import java.util.Scanner;
 import java.io.*;
 public class server
 {
     public static void main(String args[])throws IOException
     {
         int port = 3333;
-        String file = "essai.txt";
+        String filename = "essai.txt";
         if (args.length == 2){
             // we got the port and the filename
             port = Integer.parseInt(args[0]);
-            file = args[1];
+            filename = args[1];
         }
-        System.out.println("Ouverture de la connexion sur le port : " + port +".\nLecture dans le ecriture dans le fichier: " + file);
-        byte b[]=new byte[66000];
+        System.out.println("Ouverture de la connexion sur le port : " + port +".\nEcriture dans le fichier: " + filename);
+        byte data[]=new byte[66000];
         DatagramSocket dsoc=new DatagramSocket(port);
-        FileOutputStream f=new FileOutputStream(file);
+        File file = new File(filename);
+        FileOutputStream fop=new FileOutputStream(file);
         while(true)
         {
-            DatagramPacket dp=new DatagramPacket(b,b.length);
+            DatagramPacket dp=new DatagramPacket(data, data.length);
             dsoc.receive(dp);
-            System.out.println(dp.getLength());                             
-
+            fop.write(dp.getData(), 0, dp.getLength());
+            System.out.println(dp.getLength());   
         }
+        
     }
 }
 
